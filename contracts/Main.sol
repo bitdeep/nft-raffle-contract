195762340adcf2903f3c8169b005a2c20254e05c
wendel <19378023+bitdeep@users.noreply.github.com>
2022-09-03 13:01:16 -0300

    buy/cancel functions

--- contracts/Main.sol
+++ contracts/Main.sol
@@ -1,12 +1,14 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.9;
-//import "hardhat/console.sol";
+
+import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
-contract Main is Ownable, IERC721Receiver  {
+
+contract Main is Ownable, IERC721Receiver {
     using SafeERC20 for IERC20;
     using SafeERC20 for IERC721;
     address FEE_RECIPIENT1;
@@ -17,23 +19,10 @@ contract Main is Ownable, IERC721Receiver  {
         FEE_RECIPIENT2 = msg.sender;
         FEE_RECIPIENT3 = msg.sender;
     }
-    function setFeeRecipient(address a,address b,address c) public onlyOwner {
+    function setFeeRecipient(address a, address b, address c) public onlyOwner {
         FEE_RECIPIENT1 = a;
         FEE_RECIPIENT2 = b;
         FEE_RECIPIENT3 = c;
-    }
-
-    function _trigger() internal {
-        /*
-        (uint _previousBlockNumber, bytes32 _previousBlockHash) = moreRand();
-        uint total = ticketsByLottery[lottery].length;
-        uint256 _mod = total - 1;
-        uint256 _randomNumber;
-        bytes32 _structHash = keccak256(abi.encode(msg.sender, block.difficulty, gasleft(),
-            block.timestamp, _previousBlockNumber, _previousBlockHash));
-        _randomNumber = uint256(_structHash);
-        assembly {_randomNumber := mod(_randomNumber, _mod)}
-        */
     }
 
     function moreRand() public view returns (uint, bytes32) {
@@ -44,7 +33,7 @@ contract Main is Ownable, IERC721Receiver  {
         return (_previousBlockNumber, _previousBlockHash);
     }
 
-    enum STATUS {ACTIVE, FINISHED}
+    enum STATUS {ACTIVE, FINISHED, CANCELED}
     struct Raffle {
         STATUS status;
         uint max_per_user; // max raffle by user
@@ -54,21 +43,27 @@ contract Main is Ownable, IERC721Receiver  {
         address winner;
         address[] users;
         address seller;
+        uint price;
+        uint amount;
     }
+
     Raffle[] public raffles;
-    function create_raffle(uint _max_per_user, address _nft, uint _nft_id) public {
+
+    function create_raffle(uint _max_per_user, address _nft, uint _nft_id, uint _price, uint _total) public {
         require(_max_per_user > 0, "E1");
         require(_nft != address(0), "E2");
         address[] memory _users = new address[](0);
         Raffle memory raffle = Raffle({
-            status: STATUS.ACTIVE,
-            max_per_user: _max_per_user,
-            total: 0,
-            nft: _nft,
-            nft_id: _nft_id,
-            winner: address(0),
-            users: _users,
-            seller: msg.sender });
+        status : STATUS.ACTIVE,
+        max_per_user : _max_per_user,
+        total : _total,
+        nft : _nft,
+        nft_id : _nft_id,
+        winner : address(0),
+        users : _users,
+        seller : msg.sender,
+        price : _price,
+        amount : 0});
         raffles.push(raffle);
         IERC721 token = IERC721(raffle.nft);
         require(
@@ -77,7 +72,56 @@ contract Main is Ownable, IERC721Receiver  {
         );
         token.safeTransferFrom(msg.sender, address(this), raffle.nft_id);
     }
+
+    function buy(uint amount, uint raffle_id) payable public {
+        Raffle storage raffle = raffles[raffle_id];
+        require(raffle.status == STATUS.ACTIVE, "!ACTIVE");
+        require(msg.value == raffle.price * amount, "Price incorrect");
+        raffle.amount += amount;
+        for (uint i = 0; i < amount; i ++) {
+            raffle.users.push(msg.sender);
+            console.log('users=%s total=%s', raffle.users.length, raffle.total);
+            if (raffle.users.length >= raffle.total) {
+                _trigger(raffle_id);
+                break;
+            }
+        }
+    }
+
     function onERC721Received(address, address, uint256, bytes memory) public virtual override returns (bytes4) {
         return this.onERC721Received.selector;
+    }
+
+    function _trigger(uint raffle_id) internal {
+        console.log('trigger!');
+        Raffle storage raffle = raffles[raffle_id];
+        (uint _previousBlockNumber, bytes32 _previousBlockHash) = moreRand();
+        uint total = raffle.users.length;
+        uint256 _mod = total - 1;
+        uint256 _randomNumber;
+        bytes32 _structHash = keccak256(abi.encode(msg.sender, block.difficulty, gasleft(),
+            block.timestamp, _previousBlockNumber, _previousBlockHash));
+        _randomNumber = uint256(_structHash);
+        assembly {_randomNumber := mod(_randomNumber, _mod)}
+        raffle.winner = raffle.users[_randomNumber];
+        raffle.status = STATUS.FINISHED;
+        console.log('raffle.winner=%s _randomNumber=%s', raffle.winner, _randomNumber);
+        IERC721(raffle.nft).safeTransferFrom(address(this), raffle.winner, raffle.nft_id);
+        raffle.seller.call{value : raffle.amount}("");
+    }
+
+
+    function cancel(uint raffle_id) public {
+        Raffle storage raffle = raffles[raffle_id];
+        require(raffle.status == STATUS.ACTIVE, "!ACTIVE");
+        require(raffle.seller == msg.sender, "not owner");
+        for (uint i = 0; i < raffle.users.length; i ++) {
+            console.log('i=%s total=%s', i, raffle.total);
+            address user = raffle.users[i];
+            user.call{value : raffle.price}("");
+        }
+        //
+        IERC721(raffle.nft).safeTransferFrom(address(this), msg.sender, raffle.nft_id);
+        raffle.status = STATUS.CANCELED;
     }
 }
--- test/Main.js
+++ test/Main.js
@@ -1,9 +1,14 @@
 const {expect} = require("chai");
 let nft, main;
+let USER1, USER2, USER3, USER4, USER5;
 describe("Main", function () {
     beforeEach(async () => {
-        const [owner, user] = await ethers.getSigners();
-
+        const [owner, user1, user2, user3, user4, user5] = await ethers.getSigners();
+        USER1 = user1;
+        USER2 = user2;
+        USER3 = user3;
+        USER4 = user4;
+        USER5 = user5;
         const MockNFT = await ethers.getContractFactory("MockNFT");
         nft = await MockNFT.deploy();
 
@@ -13,18 +18,36 @@ describe("Main", function () {
 
     describe("Test", async () => {
         it("main", async function () {
+
             await nft.mint();
+            await nft.connect(USER1).mint();
+            await nft.connect(USER2).mint();
+            await nft.connect(USER3).mint();
+            await nft.connect(USER4).mint();
+            await nft.connect(USER5).mint();
+
             const index = await nft.index();
-            expect(index).to.equal("1");
+            expect(index).to.equal("6");
             await nft.setApprovalForAll(main.address, true);
+            await nft.connect(USER1).setApprovalForAll(main.address, true);
+            await nft.connect(USER2).setApprovalForAll(main.address, true);
+            await nft.connect(USER3).setApprovalForAll(main.address, true);
+            await nft.connect(USER4).setApprovalForAll(main.address, true);
+            await nft.connect(USER5).setApprovalForAll(main.address, true);
 
             const _max_per_user = '2';
             const _nft = nft.address;
             const _nft_id = '0';
-            await main.create_raffle(_max_per_user, _nft, _nft_id);
+            const _price = 1e18.toString();
+            const _total = 5;
+            await main.create_raffle(_max_per_user, _nft, _nft_id, _price, _total);
+
+            const rafle_id = '0';
+            await main.connect(USER1).buy(1,rafle_id, {value: _price});
+            await main.connect(USER2).buy(1,rafle_id, {value: _price});
+            await main.connect(USER3).buy(1,rafle_id, {value: _price});
+            await main.connect(USER4).buy(1,rafle_id, {value: _price});
+            await main.connect(USER5).buy(1,rafle_id, {value: _price});//
         });
-
-
     });
-
 });
